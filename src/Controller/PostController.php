b48d4c1e3354ccd612ffe611858ef3163f088c88
Alex <alexander.wozniak1@gmail.com>
2019-09-03 21:08:14 +0200

    Adding and displaying comments

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +16,35 @@ class PostController extends AbstractController
      */
     public function index(Request $request, Post $post)
     {
+        $comment = new Comment();
+
+        $commentForm = $this->createForm(CommentFormType::class, $comment);
+
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+
+            $comment->setUser($user);
+            $comment->setPost($post);
+
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', "Comment has been added.");
+
+            return $this->redirectToRoute('show_post', [
+                'id' => $post->getId()
+            ]);
+        }
+
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
             'post' => $post,
+            'comment_form' => $commentForm->createView(),
         ]);
     }
+
 }
