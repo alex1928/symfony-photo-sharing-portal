23c203ec1ae9371cef93e4b1d6f17ee071f864e8
Alex <alexander.wozniak1@gmail.com>
2019-09-10 11:45:53 +0200

    links to profile in posts and comments

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/profile/{id<\d+>}", name="profile")
      */
-    public function index()
+    public function index(User $user)
     {
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'user' => $user,
         ]);
     }
 }
