6e4745f09b336d7a15d3a8140321d1fd75a1b593
Alex <alexander.wozniak1@gmail.com>
2019-09-10 13:10:24 +0200

    Ability to edit "about me" text

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,22 +5,62 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
+
     /**
-     * @Route("/profile/{id<\d+>}", name="profile")
+     * @Route("/profile/edit", name="profile_edit")
+     */
+    public function edit(Request $request, PostRepository $postRepository)
+    {
+        $user = $this->getUser();
+        $posts = $postRepository->getLastPostsByUser($user, 15);
+
+        $form = $this->createFormBuilder($user)
+            ->add('aboutMe', TextareaType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash("success", "Yours description has been saved.");
+            return $this->redirectToRoute('profile_show', [
+                'id'=> $user->getId()
+            ]);
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+            'user' => $user,
+            'posts' => $posts,
+            'form' => $form->createView(),
+            'edit' => true
+        ]);
+    }
+
+    /**
+     * @Route("/profile/{id<\d+>}", name="profile_show")
      */
     public function index(User $user, PostRepository $postRepository)
     {
-
         $posts = $postRepository->getLastPostsByUser($user, 15);
 
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
             'user' => $user,
             'posts' => $posts,
+            'edit' => false
         ]);
     }
+
+
+
 }
